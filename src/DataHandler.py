9989213292a112b5c41cdbbf26a3d5bb9f4dc9cf
YanaiA <yanai@nanit.com>
2018-02-26 15:07:51 +0200

    split to words

--- src/DataHandler.py
+++ src/DataHandler.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
+import re
+
 
 class DataHandler:
     def __init__(self, base_folder):
@@ -17,6 +19,17 @@ class DataHandler:
         self.data['train']['cleaned'].fillna(value='none', inplace=True)
 
     def analyze(self):
-        self.data['train']['vectored'] = self.data['train']['cleaned']
+        self.data['train']['vectored'] = self.data['train']['cleaned'].apply(DataHandler.text_to_words)
+        print(self.data['train']['vectored'])
 
-
+    @staticmethod
+    def text_to_words(raw_text, remove_stopwords=False):
+        # 1. Remove non-letters, but including numbers
+        letters_only = re.sub("[^0-9a-zA-Z]", " ", raw_text)
+        # 2. Convert to lower case, split into individual words
+        words = letters_only.lower().split()
+        # if remove_stopwords:
+        #     stops = set(stopwords.words("english")) # In Python, searching a set is much faster than searching
+        #     meaningful_words = [w for w in words if not w in stops] # Remove stop words
+        #     words = meaningful_words
+        return words

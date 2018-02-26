a15b19118671ef76be76e521c12ac3179b268d6c
Shaked <shaked.dovrat@gmail.com>
2018-02-26 18:03:14 +0200

    harta, change clean

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -5,7 +5,8 @@ from DataHandler import DataHandler
 def main():
     data_handler = DataHandler(os.path.join('..', 'data'))
     data_handler.load()
-    data_handler.clean('all_comments.txt')
+    data_handler.clean('all_comments3.txt')
+    # data_handler.train_word2vec(os.path.join('..', 'data', 'all_comments.txt'), '/home/bar/extDisk/kaggle/word2vec_twitter_model/word2vec_twitter_model.bin')
 
 
 if __name__ == '__main__':
